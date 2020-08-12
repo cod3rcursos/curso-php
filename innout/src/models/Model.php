c3058b26e91b6ceaf9bc961fa4d2a71313e224e2
Wagner Costa <wcaquino@gmail.com>
2020-08-12 09:28:57 -0300

    retirando sanitização ao recuperar do banco

--- innout/src/models/Model.php
+++ innout/src/models/Model.php
@@ -1,20 +1,23 @@
 <?php
 
-class Model {
+class Model
+{
     protected static $tableName = '';
     protected static $columns = [];
     protected $values = [];
 
-    function __construct($arr, $sanitize = true) {
+    function __construct($arr, $sanitize = true)
+    {
         $this->loadFromArray($arr, $sanitize);
     }
 
-    public function loadFromArray($arr, $sanitize = true) {
-        if($arr) {
+    public function loadFromArray($arr, $sanitize = true)
+    {
+        if ($arr) {
             // $conn = Database::getConnection();
-            foreach($arr as $key => $value) {
+            foreach ($arr as $key => $value) {
                 $cleanValue = $value;
-                if($sanitize && isset($cleanValue)) {
+                if ($sanitize && isset($cleanValue)) {
                     $cleanValue = strip_tags(trim($cleanValue));
                     $cleanValue = htmlentities($cleanValue, ENT_NOQUOTES);
                     // $cleanValue = mysqli_real_escape_string($conn, $cleanValue);
@@ -25,52 +28,59 @@ class Model {
         }
     }
 
-    public function __get($key) {
-        return $this->values[$key];
+    public function __get($key)
+    {
+        return $this->values[$key] ?? null;
     }
 
-    public function __set($key, $value) {
+    public function __set($key, $value)
+    {
         $this->values[$key] = $value;
     }
 
-    public function getValues() {
+    public function getValues()
+    {
         return $this->values;
     }
 
-    public static function getOne($filters = [], $columns = '*') {
+    public static function getOne($filters = [], $columns = '*')
+    {
         $class = get_called_class();
         $result = static::getResultSetFromSelect($filters, $columns);
         return $result ? new $class($result->fetch_assoc()) : null;
     }
 
-    public static function get($filters = [], $columns = '*') {
+    public static function get($filters = [], $columns = '*')
+    {
         $objects = [];
         $result = static::getResultSetFromSelect($filters, $columns);
-        if($result) {
+        if ($result) {
             $class = get_called_class();
-            while($row = $result->fetch_assoc()) {
-                array_push($objects, new $class($row));
+            while ($row = $result->fetch_assoc()) {
+                array_push($objects, new $class($row, false));
             }
         }
         return $objects;
     }
 
-    public static function getResultSetFromSelect($filters = [], $columns = '*') {
+    public static function getResultSetFromSelect($filters = [], $columns = '*')
+    {
         $sql = "SELECT ${columns} FROM "
             . static::$tableName
             . static::getFilters($filters);
         $result = Database::getResultFromQuery($sql);
-        if($result->num_rows === 0) {
+        if ($result->num_rows === 0) {
             return null;
         } else {
             return $result;
         }
     }
 
-    public function insert() {
+    public function insert()
+    {
         $sql = "INSERT INTO " . static::$tableName . " ("
             . implode(",", static::$columns) . ") VALUES (";
-        foreach(static::$columns as $col) {
+        foreach (static::$columns as $col) {
             $sql .= static::getFormatedValue($this->$col) . ",";
         }
         $sql[strlen($sql) - 1] = ')';
@@ -78,9 +88,10 @@ class Model {
         $this->id = $id;
     }
 
-    public function update() {
+    public function update()
+    {
         $sql = "UPDATE " . static::$tableName . " SET ";
-        foreach(static::$columns as $col) {
+        foreach (static::$columns as $col) {
             $sql .= " ${col} = " . static::getFormatedValue($this->$col) . ",";
         }
         $sql[strlen($sql) - 1] = ' ';
@@ -88,40 +99,47 @@ class Model {
         Database::executeSQL($sql);
     }
 
-    public static function getCount($filters = []) {
+    public static function getCount($filters = [])
+    {
         $result = static::getResultSetFromSelect(
-            $filters, 'count(*) as count');
+            $filters,
+            'count(*) as count'
+        );
         return $result->fetch_assoc()['count'];
     }
 
-    public function delete() {
+    public function delete()
+    {
         static::deleteById($this->id);
     }
 
-    public static function deleteById($id) {
+    public static function deleteById($id)
+    {
         $sql = "DELETE FROM " . static::$tableName . " WHERE id = {$id}";
         Database::executeSQL($sql);
     }
 
-    private static function getFilters($filters) {
+    private static function getFilters($filters)
+    {
         $sql = '';
-        if(count($filters) > 0) {
+        if (count($filters) > 0) {
             $sql .= " WHERE 1 = 1";
-            foreach($filters as $column => $value) {
-                if($column == 'raw') {
+            foreach ($filters as $column => $value) {
+                if ($column == 'raw') {
                     $sql .= " AND {$value}";
                 } else {
                     $sql .= " AND ${column} = " . static::getFormatedValue($value);
                 }
             }
-        } 
+        }
         return $sql;
     }
 
-    private static function getFormatedValue($value) {
-        if(is_null($value)) {
+    private static function getFormatedValue($value)
+    {
+        if (is_null($value)) {
             return "null";
-        } elseif(gettype($value) === 'string') {
+        } elseif (gettype($value) === 'string') {
             return "'${value}'";
         } else {
             return $value;
