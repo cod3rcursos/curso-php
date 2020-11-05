72844c29001549f9d3caf83fbd83ecc8bb8c1fed
Wagner Aquino <wcaquino@gmail.com>
2020-11-05 11:50:11 -0300

    criptografando a senha do usuário

--- innout/src/models/User.php
+++ innout/src/models/User.php
@@ -19,6 +19,7 @@ class User extends Model {
         $this->validate();
         $this->is_admin = $this->is_admin ? 1 : 0;
         if(!$this->end_date) $this->end_date = null;
+        $this->password = password_hash($this->password, PASSWORD_DEFAULT);
         return parent::insert();
     }
 
